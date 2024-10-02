3d805258ecb1b7cff0a93835df055144813e9204
Beste Çeltek <besteceltek@gmail.com>
2024-10-02 15:57:29 +0300

    Removed Active Page Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import './App.css'
-import ActivePageProvider from './context/ActivePageProvider'
 import UpdatePageProvider from './context/UpdatePageProvider'
 import LoadingProvider from './context/LoadingProvider'
 import Navbar from './components/Navbar/Navbar'
-import Main from './components/Main/Main'
+import MainArea from './components/MainArea/MainArea'
 
 function App() {
   return (
-    <ActivePageProvider>
-      <UpdatePageProvider>
-        <LoadingProvider>
+    <UpdatePageProvider>
+      <LoadingProvider>
+        <header>
           <Navbar />
-          <Main />
-        </LoadingProvider>
-      </UpdatePageProvider>
-    </ActivePageProvider>
+        </header>
+        <div className='main-area'>
+          <MainArea />
+        </div>
+      </LoadingProvider>
+    </UpdatePageProvider>
   )
 }
 
