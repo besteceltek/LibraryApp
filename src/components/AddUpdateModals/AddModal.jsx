f124b7b9567a15ee1222bae6a9dcbef7e5bc1440
Beste Çeltek <besteceltek@gmail.com>
2024-10-02 15:58:05 +0300

    Created Modal component for add/update

---
+++ src/components/AddUpdateModals/AddModal.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import './Modal.css'
+import Button from '@mui/material/Button';
+import AppButton from '../Utils/AppButton'
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
+
+function AddModal({ dialogContent, prop, addFunction }) {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleModalOpen = () => {
+    setModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <div>
+      <div className="add-button">
+        <AppButton text={`Add ${prop}`} buttonFunc={handleModalOpen}/>
+      </div>
+      <Dialog
+        id='modal'
+        open={modalOpen}
+        onClose={handleModalClose}
+        PaperProps={{
+          component: 'form',
+          onSubmit: (event) => {
+            event.preventDefault()
+            addFunction()
+            handleModalClose()
+          }
+        }}
+      >
+        <DialogTitle>Add {prop}</DialogTitle>
+        {dialogContent}
+        <DialogActions id='actions'>
+          <Button onClick={handleModalClose}>Cancel</Button>
+          <Button type="submit">Add</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  )
+}
+
+export default AddModal
---
+++ src/components/AddUpdateModals/UpdateModal.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import './Modal.css'
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
+
+function UpdateModal({ dialogContent, prop, updateFunction, updateModalOpen, handleModalClose }) {
+
+  return (
+    <>
+      <Dialog
+        id='modal'
+        open={updateModalOpen}
+        onClose={handleModalClose}
+        PaperProps={{
+          component: 'form',
+          onSubmit: (event) => {
+            event.preventDefault()
+            updateFunction()
+            handleModalClose()
+          }
+        }}
+      >
+        <DialogTitle>Update {prop}</DialogTitle>
+        {dialogContent}
+        <DialogActions id='actions'>
+          <Button onClick={handleModalClose}>Cancel</Button>
+          <Button type="submit">Update</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  )
+}
+
+export default UpdateModal
