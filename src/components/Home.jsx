e5d40e99d05a6667763afc9edfd40c6c84f34850
Beste Çeltek <besteceltek@gmail.com>
2024-10-04 00:18:26 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,10 @@
-import home from '../../public/home.jpeg'
+import home from '../../public/home.png'
 
 function Home() {
   return (
     <div>
         <h1 style={{ color: 'var(--text-color)'}}>WELCOME TO BEST READS</h1>
-        <img src={hom2} alt="" style={{width:'1200px'}} />
+        <img src={home} alt="" style={{width:'1200px', overflowY:'hidden'}} />
     </div>
   )
 }
